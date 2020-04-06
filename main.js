5fc01a76b27fbbe8d4a53d127766855f37a2a6c1
Chris <krzysiek9k@gmail.com>
2020-04-06 10:59:29 +0200

    finished

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 const btn = document.querySelector('button');
-const div = document.querySelector('div');
-
+const div = document.querySelector('div')
 const names = ["Jagienka", "Dobrawa (Dobrusia)", "Daria", "Asia", "Kira", "Marysia", "Anastazja", "Kasia"];
 
-const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę", "Szczerze uważam", "Na 100% twierdzę", "Jestem pewien"];
 
-// console.log(names[index]);
 
 const nameGenerator = () => {
- const indexName = Math.floor(Math.random() * names.length);
- // const indexPrefix = Math.floor(Math.random() * prefixs.length);
- // div.textContent = `${prefixs[indexPrefix]}, że najlepsze będzie imię ${names[indexName]}`
- div.textContent = `${prefixs[Math.floor(Math.random() * prefixs.length)]}, że najlepsze będzie imię ${names[indexName]}`
+    //losowanie liczby od 0 do 6.(999)
+    const index = Math.floor(names.length * Math.random())
+    div.textContent = `Wybranę imię to ${names[index]}`
 }
+
 
 btn.addEventListener('click', nameGenerator)
